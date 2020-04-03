04bf320e971f2e5a89f42214cf8e0a616c779970
RikBala <rik280491@googlemail.com>
2020-04-03 18:02:16 +0100

    Rik is a crazy fucker

--- src/index.js
+++ src/index.js
@@ -1,56 +1,76 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dogsAPI = " http://localhost:3000/dogs";
-  const table = document.querySelector("#table-body");
-  const formName = document.querySelector("input[name='name']");
-  const formBreed = document.querySelector("input[name='breed']");
-  const formSex = document.querySelector("input[name='sex']");
-  let selectedDog;
+const dogsAPI = "http://localhost:3000/dogs"
 
-  const init = () => {
-    fetch(dogsAPI)
-      .then(resp => resp.json())
-      .then(dogs => renderDogs(dogs));
-  };
+const table = document.querySelector(".margin")
+const tableBody = document.querySelector("#table-body")
+const form = document.querySelector("#dog-form")
+const formName = document.querySelector("input[name = 'name']")
+const formBreed = document.querySelector("input[name = 'breed']")
+const formSex = document.querySelector("input[name = 'sex']")
+const dogForm = document.querySelector("#dog-form")
+let selectedDog;
+let selectedRow;
 
-  const renderDogs = dogs => {
-    dogs.forEach(dog => renderDog(dog));
-  };
+const fetchDogs = () => {
+  fetch(dogsAPI)
+  .then(resp => resp.json())
+  .then(dogs => renderDogs(dogs))
 
-  const renderDog = dog => {
-    const dogRow = document.createElement("tr");
-    const dogName = document.createElement("th");
-    const dogBreed = document.createElement("th");
-    const dogSex = document.createElement("th");
-    const dogEdit = document.createElement("button");
+}
 
-    dogName.className = "padding center";
-    dogName.innerText = dog.name;
-    dogBreed.className = "padding center";
-    dogBreed.innerText = dog.breed;
-    dogSex.className = "padding center";
-    dogSex.innerText = dog.sex;
-    dogEdit.className = "padding center";
-    dogEdit.innerText = "Edit";
 
-    dogEdit.addEventListener("click", () => {
-      formName.value = dog.name;
-      formBreed.value = dog.breed;
-      formSex.value = dog.sex;
-      selectedDog = dog;
-    });
+const renderDogs = (dogs) => {
+  dogs.forEach(renderDog)
+}
 
-    dogRow.append(dogName, dogBreed, dogSex, dogEdit);
-    table.append(dogRow);
-  };
+const renderTableRow = dog => {
+  const dogName = document.createElement("th")
+  dogName.className = "padding center"
+  dogName.innerText = dog.name
+  const dogBreed = document.createElement("th")
+  dogBreed.className = "padding center"
+  dogBreed.innerText = dog.breed
+  const dogSex = document.createElement("th")
+  dogSex.className = "padding center"
+  dogSex.innerText = dog.sex
 
-  const dogForm = document.querySelector("#dog-form");
+  const dogEdit = document.createElement("button")
+  dogEdit.innerText = "Edit"
 
-  const handleFormSubmit = event => {
-    event.preventDefault();
+  return [dogName, dogBreed, dogSex, dogEdit]
+}
 
-    if (!selectedDog) return console.error("no dog selected");
+const renderDog = (dog) => {
+ const dogRow = document.createElement("tr")
+  
+ const [dogName, dogBreed, dogSex, dogEdit] = renderTableRow(dog)
+  
+  
+  dogEdit.className = "padding center"
+  dogEdit.innerText = "Edit"
+  dogEdit.style.background = "pink"
+  
+  dogEdit.addEventListener("click", () => {
+    formName.value = dog.name 
+    formBreed.value = dog.breed
+    formSex.value = dog.sex
+    selectedDog = dog
+    selectedRow = dogRow
+  }
+  )
+ 
+  dogRow.append(dogName, dogBreed, dogSex, dogEdit)
+  tableBody.append(dogRow)
+  
+  
+}
 
-    const configObject = {
+dogForm.addEventListener("submit",e => handleFormSubmit(e))
+
+  
+const handleFormSubmit = (event, dogName, dogBreed, dogSex, dogEdit) => {
+      event.preventDefault();
+    
+     const configObject = {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -61,16 +81,19 @@ document.addEventListener("DOMContentLoaded", () => {
         breed: event.target.breed.value,
         sex: event.target.sex.value
       })
+      
     };
-    console.log("fetching...");
-    fetch(`${dogsAPI}/${selectedDog.id}`, configObject)
-      .then(res => res.json())
-      .then(dog => {
-        renderDog(dog);
-        selectedDog = null;
-      });
-  };
-  dogForm.addEventListener("submit", handleFormSubmit);
+    
+    fetch(`http://localhost:3000/dogs/${selectedDog.id}`, configObject)
+    .then(resp => resp.json())
+    .then((updatedDog) => {
 
-  init();
-});
+      const [dogName, dogBreed, dogSex, dogEdit] = renderTableRow(updatedDog)
+      selectedRow.innerText = ''
+      selectedRow.append(dogName,dogBreed, dogSex, dogEdit)
+
+    })
+}
+
+
+fetchDogs();
